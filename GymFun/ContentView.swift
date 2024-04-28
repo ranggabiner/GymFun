78f9baa552aef72c17daea14a9636ba0212c8a37
ranggaupnvj <2310501014@mahasiswa.upnvj.ac.id>
2024-04-28 18:04:18 +0700

    Initial Commit

---
+++ GymFun/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  GymFun
+//
+//  Created by Rangga Biner on 28/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CameraWithPosesAndOverlaysView()
+    }
+}
